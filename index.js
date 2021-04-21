c9d5f7935bb27b78a9a26a58a28547052b46296a
tobybrave <leutoby@gmail.com>
2021-04-21 09:34:41 +0100

    writes to file if dir already exist

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const fs = require("fs")
 const axios = require("axios")
 
+const writeFileToResultDir = (data) => {
+  return fs.writeFile("./result/posts.json", JSON.stringify(data), (err) => {
+    if (err) throw new Error("could not create file")
+    console.log("operation was successful")
+  })
+}
+
 const main = async () => {
   try {
     const response = await axios.get("http://jsonplaceholder.typicode.com/posts")
     const post = response.data
     
     fs.mkdir("result", (err) => {
-      if (err) throw new Error("attempt to create directory failed")
+      if (err) {
+        if (err.code === 'EEXIST') {
+          writeFileToResultDir(post)
+          return
+        }
+        throw new Error(err)
+      }
       
-      fs.writeFile("./result/posts.json", JSON.stringify(post), (err) => {
-        if (err) throw new Error("could not create file")
-        console.log("operation was successful")
-      })
+      writeFileToResultDir(post)
     })
   } catch (err) {
     console.error(`${err.code}: an error while trying to connect to ${err.hostname} `)
